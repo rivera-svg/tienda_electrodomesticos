4c48095000c5c3351818abc30472fc9ec4f1bc62
rivera-svg <riveraaxel926@gmail.com>
2025-10-29 21:14:19 -0500

    Add files via upload

---
+++ eureka-server/src/test/java/pe/edu/cibertec/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
